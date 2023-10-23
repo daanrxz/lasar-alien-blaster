d268a3b8e13073aa3b16b7eb7f42f7e2905f2365
FaresQaddoura29 <faresqaddoura@gmail.com>
2023-10-23 12:05:13 +0100

    character movement right and left

--- game.js
+++ game.js
@@ -1,9 +1,54 @@
-const player = document.getElementById("main-character");
-const gameContainer = document.getElementById("main-container")
+
+
+window.onload = ()=>{
+    //put startGame() in the button after
+    startGame();
+}
+
+function startGame(){
+
+    const game = new Game();
+    game.start()
+
+    function keydownFunc(e){
+
+        const key = e.key;
+        const keys = [ "ArrowLeft", "ArrowRight"];
+        if(keys.includes(key)){
+            e.preventDefault();
+        }
+        if(game){
+            switch(key){
+                case "ArrowLeft":
+                    game.player.directionX = -3;
+                    break;
+                case "ArrowRight":
+                    game.player.directionX = 3;
+            }
+        }
+    }
+    function keyupFunc(e){
+        const key = e.key;
+        const keys = [ "ArrowLeft", "ArrowRight"];
+        if(keys.includes(key)){
+            e.preventDefault();
+        }
+        if(game){
+            switch(key){
+                case "ArrowLeft":
+                    game.player.directionX = 0;
+                    break;
+                case "ArrowRight":
+                    game.player.directionX = 0;
+            }
+        }
+    }
+    window.addEventListener("keydown", keydownFunc)
+    window.addEventListener("keyup", keyupFunc)
+}
 
 
 
-window.addEventListener("keydown", moveCharacter);
 
 class Player{
     constructor(screen, left, width, height, imgSrc){
@@ -13,49 +58,59 @@ class Player{
         this.width = width;
         this.height = height;
         this.directionX = 0;
-        this.directionY = 0;
         //player element
-        this.element = player;
+        this.element = document.createElement("div");
+        this.element.id = "main-character";
+        this.screen.appendChild(this.element)
     }
     move(){
+        console.log("tedst");
+
         //update the tank's position depending on the direction
         this.left += this.directionX;
-        
+        //right and left
+        if(this.left + this.width >= this.screen.offsetWidth){
+            this.left = this.screen.offsetWidth - this.width
+        }
+        else if(this.left <= 0){
+            this.left = 0;
+        }
+        this.updatePosition();
+
+    }
+    updatePosition(){
+        this.element.style.left = this.left + "px";
+
     }
 }
 
-function moveCharacter(e){
-    switch(e.key){
-        case "ArrowLeft":
-            move(-1, 1)
-            break;
-        case "ArrowRight":
-            move(1, 1)
-            break;
+class Game{
+    constructor(){
+        this.screen = document.getElementById("main-container");
+        this.player = new Player(this.screen, 400, 70, 30)
+        this.height = 700;
+        this.width = 800;
+        this.obstacles = [];
+        this.lives = 5;
+        this.gameIsOver = false;
+        this.loadingObstacle = false;
+
+    }
+    start(){
+
+        this.gameLoop();
+    }
+    gameLoop(){
+        if(this.gameIsOver){ return }
+        this.update();
+        
+        window.requestAnimationFrame(()=>{
+            this.gameLoop()
+            }
+        ) 
+    }
+    update(){
+        this.player.move()
     }
 
-}
-
-function move(direction, speed){
-        const containerStyle = getComputedStyle(gameContainer);
-        const containerWidth = parseInt(containerStyle.width);
-        const playerStyle = getComputedStyle(player);
-        const playerLeft = parseInt(playerStyle.left)
-        const playerWidth = parseInt(playerStyle.width);
-
-        
-
-        if(direction > 0){
-            player.style.left = playerLeft + speed + "px"
-        }
-
-        if(direction < 0){
-            player.style.left = playerLeft - speed + "px"
-        }
-        if(playerLeft + playerWidth > containerWidth){
-            player.style.left = (containerWidth - playerWidth) + "px";
-        }
-        else if(playerLeft < 0 ){
-            player.style.left = 0 +"px";
-        }
 }
